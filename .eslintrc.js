5a1741ea3929199c6a9f253190f43454a0a4cd84
Christian Bravo <christian151.bravo@gmail.com>
2021-01-17 21:47:39 -0600

    :wrench: (Adding initial config files)

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+
+  'settings': {
+    'react': {
+      'pragma': 'React',
+      'fragment': 'Fragment',
+      'version': 'detect',
+    },
+  },
+
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+  }
+};
