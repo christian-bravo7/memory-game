4883b2df99b4f9c19dd8464243c4b7f8b75fa0ff
Christian Bravo <christian151.bravo@gmail.com>
2021-02-17 22:41:42 -0600

    :wrench: (Move app context wrappers into index.js)

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ import App from '@/App';
 import theme from '@/theme';
 import reportWebVitals from '@/reportWebVitals';
 
+import { GameSettingsContextProvider } from '@/context/GameSettingsContext';
+import { ModalContextProvider } from '@/context/ModalContext';
+
 import '@/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <ModalContextProvider>
+        <GameSettingsContextProvider>
+          <App />
+        </GameSettingsContextProvider>
+      </ModalContextProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
